a7fc5c79ebf6b6a55aafa80312e7e7a915eaf59f
Chris Shepherd <chris@chrisshepherd.me>
2015-07-22 23:54:04 +0100

    Added documentation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react/addons';
 
+/**
+ * Wraps the passed in `Component` in a higher-order component. It can then
+ * measure the performance of every render of the `Component`.
+ *
+ * Can also be used as an ES2016 decorator.
+ * @param  {ReactComponent} Component the component to wrap
+ * @return {ReactComponent}           the wrapped component
+ */
 export default function perf(Component) {
   if(process.env.NODE_ENV !== 'production') {
     if (!(Component.prototype instanceof React.Component)) {
