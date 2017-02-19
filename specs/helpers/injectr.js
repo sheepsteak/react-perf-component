2cff6b2cd7b4e5315e2034867fc10735e1b40d41
Chris Shepherd <chris@chrisshepherd.me>
2017-02-19 14:03:29 +0000

    Switch to Jest and latest eslint-config-airbnb

--- specs/helpers/injectr.js
+++
@@ -1,8 +0,0 @@
-require('babel-core/register');
-var injectr = require('injectr');
-
-injectr.onload = function(filename, contents) {
-  return require('babel-core').transform(contents, {
-    filename: filename
-  }).code;
-};
---
+++ src/__tests__/index.js
@@ -0,0 +1,56 @@
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import perf from '../';
+
+describe('perf higher-order component', () => {
+  let ReactPerf;
+
+  beforeEach(() => {
+    jest.resetModules();
+    ReactPerf = jest.genMockFromModule('react-addons-perf');
+    jest.setMock('react-addons-perf', ReactPerf);
+  });
+
+  it('wraps the passed in component when not in production', () => {
+    const MyComp = () => <div className="my-comp" />;
+    const MyCompPerf = perf(MyComp);
+
+    const result = mount(<MyCompPerf testProp="1" />);
+
+    expect(toJson(result)).toMatchSnapshot();
+  });
+
+  it('does not wrap the passed in component when in production', () => {
+    const MyComp = () => <div className="my-comp" />;
+    const NODE_ENV = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+    const MyCompPerf = perf(MyComp);
+    process.env.NODE_ENV = NODE_ENV;
+
+    const result = mount(<MyCompPerf testProp="1" />);
+
+    expect(toJson(result)).toMatchSnapshot();
+  });
+
+  it('starts ReactPerf on mount', () => {
+    const MyComp = () => <div className="my-comp" />;
+    const MyCompPerf = perf(MyComp);
+
+    mount(<MyCompPerf />);
+
+    expect(ReactPerf.start).toHaveBeenCalled();
+  });
+
+  it('collects results and starts again in update', () => {
+    const MyComp = () => <div className="my-comp" />;
+    const MyCompPerf = perf(MyComp);
+
+    const result = mount(<MyCompPerf />);
+    result.setProps();
+
+    expect(ReactPerf.getLastMeasurements).toHaveBeenCalled();
+    expect(ReactPerf.printWasted).toHaveBeenCalled();
+    expect(ReactPerf.start).toHaveBeenCalled();
+  });
+});
